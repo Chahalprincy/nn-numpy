a13fc766ffbee806c55d3ff6bdd3cef4bf76ac76
Abhishek Kadian <abhishekkadiyan@gmail.com>
2021-05-03 10:22:25 -0700

    Test forward, backward implementation

--- nn/loss.py
+++ nn/loss.py
@@ -4,8 +4,10 @@ class loss():
     def forward(self,y,inp):
         self.y = y
         self.inp = inp
-        self.loss = self.inp[y]*np.log(self.inp[y])
-        diff_loss = np.zeros(self.inp.shape)
-        diff_loss[y] = -np.log(self.inp[y])-1
-        return diff_loss
+        loss = -self.inp[y]*np.log(self.inp[y])
+        return loss
         
+    def backward(self):
+        diff_loss = np.zeros(self.inp.shape)
+        diff_loss[self.y] = -np.log(self.inp[self.y])-1
+        return diff_loss
